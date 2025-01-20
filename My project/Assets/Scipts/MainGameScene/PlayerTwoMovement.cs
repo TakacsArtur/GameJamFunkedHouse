73cf513441b27797c29641b3e848564bde00a727
Takacs Artur <takacs.artur@outlook.com>
2025-01-20 18:45:08 +0100

    started implementing movement

--- My project/Assets/Scipts/MainGameScene/PlayerTwoMovement.cs
+++ My project/Assets/Scipts/MainGameScene/PlayerTwoMovement.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class PlayerTwoMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    GameObject PlayerHand = new GameObject();
-    public int speed = 5;
+    GameObject PlayerHand;
+    public string playerHandName = "Hand_placeholder";
+    public int moveHorizontalSpeed = 5;
+    public int turnSpeed = 5;
     void Start()
     {
-        
+        PlayerHand = GameObject.Find(playerHandName);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Horizontal Axis value " + Input.GetAxis("Horizontal"));
+        Debug.Log("Axis value " + Input.GetAxis("Vertical"));
+        Debug.Log("Player rotation " + PlayerHand.transform.rotation.x + " " + PlayerHand.transform.rotation.y + " " + PlayerHand.transform.rotation.z);
+
+        //movement
+        PlayerHand.transform.position = new Vector3(PlayerHand.transform.rotation.y * PlayerHand.transform.position.x+ Input.GetAxis("Vertical") * moveHorizontalSpeed,0,0);
+        //tank control
+        PlayerHand.transform.Rotate(PlayerHand.transform.rotation.x, Input.GetAxis("Horizontal")*turnSpeed, PlayerHand.transform.rotation.z);
+        Debug.Log("Corrected value " + (Input.GetAxis("Horizontal")*turnSpeed));
     }
 }
