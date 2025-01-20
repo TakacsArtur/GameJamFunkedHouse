69d07c7852afc94696c6128d037ffa5685306af0
Takacs Artur <takacs.artur@outlook.com>
2025-01-20 18:02:17 +0100

    created the basis of the project. use 2022,3,53f1

---
+++ My project/Assets/Scipts/MainGameScene/PlayerTwoMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwoMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject PlayerHand = new GameObject();
+    public int speed = 5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
